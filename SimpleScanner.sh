9894796fed3107dce04bcaa5998948ded413ef4b
gus <64370910+gus3rmr@users.noreply.github.com>
2021-11-04 15:08:54 -0600

    Update SimpleScanner.sh

--- SimpleScanner.sh
+++ SimpleScanner.sh
@@ -3,16 +3,41 @@ source bargs.sh "$@"
 function subdomain_scanner($domain $wordlist)
 
 {
-  echo "Target domain: " $domain
+  echo ""
+  echo -e "-Target domain: ${domain}\n"
   discovered_subdomains=()
+  subdomains_file_name=$domain'_subdomains.txt'
   input=$wordlist
   while IFS= read -r line
   do
     subdomain=$line
-    new_url=$subdomain'.'$subdomain
-    echo $new_url
+    new_url=$subdomain'.'$domain
+    status_code=$(curl -o /dev/null -s -w "%{http_code}\n" https://$new_url)
+    if [[ "$status_code" -eq 200 || "$status_code" -eq 301 ]]
+    then
+      echo '[*] Found: '$new_url', status: '$status_code
+      discovered_subdomains+="\n${new_url}"
+    else
+      continue
+    fi
   done < "$input"
+  echo -e "\n-Discovered subdomains:"
+  echo -e "${discovered_subdomains}"
+  echo -e "\nCreating or updating the file with the subdomains found..."
 
+  if [[ -f "$subdomains_file_name" ]]
+  then
+    echo "Updating file..."
+    echo "$discovered_subdomains" > "$subdomains_file_name"
+    echo "File $subdomains_file_name has been updated."
+  else
+    echo "Creating file..."
+    touch "$subdomains_file_name"
+    echo "$discovered_subdomains" > "$subdomains_file_name"
+    echo "File $subdomains_file_name has been created."
+  fi
+
+  echo ""
 }
 
 subdomain_scanner
